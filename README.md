64ae267d89cb09375235c4ed2a1b9e82574137d5
sergiy-chumachenko <chumachenko.sergiy@gmail.com>
2018-04-16 21:28:54 +0300

    [UPDATE] Building The Convolutional Neural Network (CNN)

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # dogs_vs_cats_image_classification
+
+Training Set: 4000 images for Dogs, 4000 images for Cats
+Test Set: 1000 images for Dogs, 1000 images for Cats
--- convolutional_neural_networks.py
+++ convolutional_neural_networks.py
@@ -8,3 +8,37 @@
 
 # Installing Keras
 # pip install --upgrade keras
+
+# Part 1 - Building The Convolutional Neural Network (CNN)
+# Importing the Keras libraries and packages
+#%%
+from keras.models import Sequential
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+
+# Initialize the CNN
+#%%
+classifier = Sequential()
+
+# Step 1 - Convolution
+#%%
+classifier.add(Convolution2D(64, (3, 3), input_shape=(64, 64, 3), activation='relu'))
+
+# Step 2 - Pooling
+#%%
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
+
+# Step 3 - Flattening
+#%%
+classifier.add(Flatten())
+
+# Step 4 - Full connection
+#%%
+classifier.add(Dense(units=128, activation='relu'))
+classifier.add(Dense(units=1, activation='sigmoid'))
+
+# Compiling the CNN
+#%%
+classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
