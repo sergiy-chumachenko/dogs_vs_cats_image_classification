755b4a505018a07d04bf27ef8886afbdb27cc96d
sergiy-chumachenko <chumachenko.sergiy@gmail.com>
2018-04-17 22:01:53 +0300

    [UPDATE] parameters changed

--- convolutional_neural_networks.py
+++ convolutional_neural_networks.py
@@ -79,7 +79,7 @@ test_set = test_datagen.flow_from_directory(
 #%%
 classifier.fit_generator(
         training_set,
-        steps_per_epoch=1000,
-        epochs=3,
+        steps_per_epoch=8000,
+        epochs=25,
         validation_data=test_set,
         validation_steps=2000)
